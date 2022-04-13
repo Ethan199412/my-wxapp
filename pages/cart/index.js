0906fef7e25437d9e89c591671c0f8b2107d06ed
lijiahao043 <lijiahao043@ke.com>
2022-04-13 20:55:17 +0800

    4/13

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -7,6 +7,35 @@ Page({
   data: {
 
   },
+  handleChooseAddress() {
+    // 获取权限状态
+    wx.getSetting({
+      success: (result) => {
+        const scopeAddress = result.authSetting["scope.address"]
+        if (scopeAddress == true || scopeAddress == undefined) {
+          wx.chooseAddress({
+            success: res => {
+              console.log(res)
+            }
+          })
+        }
+        else {
+          wx.openSetting({
+            success: (res1) => {
+              wx.chooseAddress({
+                success: res2 => {
+                  console.log(res2)
+                }
+              })
+            },
+
+          });
+
+        }
+      },
+    });
+
+  },
 
   /**
    * 生命周期函数--监听页面加载
@@ -15,52 +44,5 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
